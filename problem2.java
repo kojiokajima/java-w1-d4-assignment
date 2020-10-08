650baa46efdd273fb3901be2aebf59d9ea27c77e
wanko <kouji124@w4.dion.ne.jp>
2020-10-09 02:44:31 +0900

    added a comment

--- problem2.java
+++ problem2.java
@@ -6,20 +6,21 @@ public class problem2 {
         System.out.println("\t\t\t\tMultiplication Table");
 
         // setting up header colums
-        for(int i = 1; i <=9; i++) {
-                System.out.print("\t " + i);
+        for (int i = 1; i <= 9; i++) {
+            System.out.print("\t " + i);
         }
         System.out.println("");
         System.out.println("--------------------------------------------------------------------------");
 
-        for(int j = 1; j <= 9; j++) {
+        for (int j = 1; j <= 9; j++) {
             // setting up header rows
             System.out.print(j + "  |" + "\t");
 
             // display the elements
-            for(int k = 1; k <= 9; k++) {
-                if(j * k < 10){
-                    System.out.print(" " +(k * j) + "\t");
+            for (int k = 1; k <= 9; k++) {
+                if (j * k < 10) {
+                    // right alignment
+                    System.out.print(" " + (k * j) + "\t");
                 } else {
                     System.out.print((k * j) + "\t");
                 }
