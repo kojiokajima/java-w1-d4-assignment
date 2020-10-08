30ccaf81b3b5c94184ff4ca1bd5018e7f98891e7
wanko <kouji124@w4.dion.ne.jp>
2020-10-09 02:22:56 +0900

    added frame of table

--- problem2.java
+++ problem2.java
@@ -1,10 +1,23 @@
 public class problem2 {
 
     public static void main(String[] args) {
+        
+        // setting a title
+        System.out.println("\t\t\t\tMultiplication Table");
 
-        for(int i = 1; i <= 9; i++) {
-            for(int j = 1; j <= 9; j++) {
-                System.out.print((j * i) + "\t");
+        // setting up header colums
+        for(int i = 1; i <=9; i++) {
+                System.out.print("\t" + i);
+        }
+        System.out.println("");
+        System.out.println("--------------------------------------------------------------------------");
+
+        // display the elements
+        for(int j = 1; j <= 9; j++) {
+            // setting up header rows
+            System.out.print(j + "  |" + "\t");
+            for(int k = 1; k <= 9; k++) {
+                System.out.print((k * j) + "\t");
             }
             System.out.println("");
         }
